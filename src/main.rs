3a04f66354f71220ab43095f325c5705c2e7b3e4
Clyne Fuentes <clippefuentes@gmail.com>
2022-08-26 20:45:34 -0600

    add invaders

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{error::Error, time::Duration, sync::mpsc, thread};
+use std::{error::Error, time::{Duration, Instant}, sync::mpsc, thread};
 use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
     cursor::{Hide, Show},
     event::{self, Event, KeyCode}
 };
-use invaders::{frame::{self, new_frame}, render};
+use invaders::{frame::{self, new_frame, Drawable}, render, player::Player, invaders::{Invader, Invaders}};
 use rusty_audio::Audio;
 use std::io;
 
@@ -42,25 +42,49 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
     // Game loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per frame init
-        let curr_frame = new_frame();
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
 
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
-                    }
+                    },
                     _ => {}
                 }
             }
         }
 
+        // update
+        player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
+
         // Draw and render
-       let _ = render_tx.send(curr_frame);
-       thread::sleep(Duration:: from_millis(1));
+        // player.draw(&mut curr_frame);
+        // invaders.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration:: from_millis(1));
     }
     // Cleanup
     drop(render_tx);
